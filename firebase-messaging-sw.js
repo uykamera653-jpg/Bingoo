704c921ecd6c0f8022e3ada2a88a01b4383837d3
uykamera653-jpg <uykamera653@gmail.com>
2025-09-03 10:42:16 +0500

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,29 @@
-console.log("Firebase SW ishladi");
+// Import firebase scripts
+importScripts("https://www.gstatic.com/firebasejs/9.6.11/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.6.11/firebase-messaging-compat.js");
+
+// Firebase config - SENING PROYEKTDAN OLGAN CONFIG
+firebase.initializeApp({
+  apiKey: "AIzaSyExample-Your-Real-ApiKey",
+  authDomain: "topildi-12345.firebaseapp.com",
+  projectId: "topildi-12345",
+  storageBucket: "topildi-12345.appspot.com",
+  messagingSenderId: "323891530719",
+  appId: "1:323891530719:web:abcd1234efgh5678",
+  measurementId: "G-XXXXXXX"
+});
+
+// Messaging init
+const messaging = firebase.messaging();
+
+// Background xabarlar kelganda ko‘rsatish
+messaging.onBackgroundMessage((payload) => {
+  console.log("Push xabar keldi: ", payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: "/icon.png"
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
